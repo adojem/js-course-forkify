71e2985eb54cca0b3e21bdfcf1a7b06325d1c66a
adojem <adojemcovix@gmail.com>
2018-08-26 14:25:07 +0900

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import num from './test';
+const x = 23;
+
+console.log(`I imported ${num} from module called test.js! Variable x is ${x}`);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+   entry: ['babel-polyfill', './src/js/index.js', './src/styles/style.scss'],
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.js'
+   },
+   devServer: {
+      contentBase: './dist'
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         filename: 'index.html',
+         template: './src/index.html'
+      }),
+      new MiniCSSExtractPlugin({
+         filename: 'css/style.css'
+      })
+   ],
+   devtool: 'source-map',
+   module: {
+      rules: [
+         {
+            test: /\.s?css$/,
+            use: [
+               MiniCSSExtractPlugin.loader,
+               {
+                  loader: 'css-loader',
+                  options: {
+                     sourceMap: true
+                  }
+               },
+               {
+                  loader: 'postcss-loader',
+                  options: {
+                     sourceMap: true,
+                     plugisn: [require('autoprefixer')({ grid: true })]
+                  }
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sourceMap: true
+                  }
+               },
+               'csscomb-loader'
+            ]
+         },
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+               loader: 'babel-loader'
+            }
+         }
+      ]
+   }
+};
