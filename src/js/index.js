821e5f9aaa1817e5fd21a40986bd791937463bab
adojem <adojemcovix@gmail.com>
2018-08-27 17:57:40 +0900

    Building the Search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/** Global state of the app
+ * - Search object
+ * - Current object
+ * - Shopping list object
+ * - Liked receipe
+ */
+const state = {};
+
+const controlSearch = async () => {
+   // Get query from view
+   const query = 'pizza'; // Todo
+
+   if (query) {
+      // New search object and to state
+      state.search = new Search(query);
+
+      // Prepqre UI for results
+
+      // Search for recipes
+      await state.search.getResults();
+
+      // Render results on UI
+      console.log(state.search.result);
+   }
+};
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+   e.preventDefault();
+   controlSearch();
+});
